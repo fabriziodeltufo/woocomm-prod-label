848f9ce50507e489d828a041a0e5299e39c1bade
FABRIZIO DEL TUFO <neo@WAIKIKI.local>
2024-01-13 11:38:59 +0100

    new plugin header details

--- woocomm-prod-label.php
+++ woocomm-prod-label.php
@@ -1,16 +1,17 @@
 <?php
 /*
-Plugin Name: WooCommerce Product Label
-Plugin URI: https://github.com/fabriziodeltufo
-Description: Input text (Name To Print) to customize a product's label.
-Version: 2.0.0
-Contributors:
-Author: Fabrizio Del Tufo
-Author URI: https://github.com/fabriziodeltufo
-License: GPLv2 or later
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: wooprolab
-Domain Path:  /languages
+* Plugin Name:        WooCommerce Product Label
+* Plugin URI:         https://wewebby.gumroad.com/
+* Description:        Input text (Name To Print) to customize a product's label.
+* Version:            1.0.0
+* Requires at least:  6.4.2
+* Requires PHP:       7.4.33
+* Author:             WeWebby.com
+* Author URI:         https://wewebby.com
+* License:            GPLv2 or later
+* License URI:        https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain:        wooprolab
+* Domain Path:        /languages
 */
 
 // If this file is called directly, abort.
